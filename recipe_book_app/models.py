d9f26dd5a494aef0451877bc2c4b252b710bc577
Eugene <e.kuzmenko@dunice.net>
2019-05-17 17:47:27 +0300

    Add functionality to search recipes by their inputs and outputs

--- recipe_book_app/models.py
+++ recipe_book_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
 
@@ -17,7 +18,7 @@ class Item(models.Model):
 class RecipeItem(models.Model):
     is_result = models.BooleanField(default=False)
     qty = models.IntegerField(null=False)
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='recipes')
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='items')
 
     def __str__(self):
--- recipe_book_app/views.py
+++ recipe_book_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from rest_framework.viewsets import ModelViewSet
 from recipe_book_app.models import Recipe, RecipeItem, Item
 from recipe_book_app.serializers import (
@@ -12,6 +13,19 @@ from recipe_book_app.serializers import (
 class RecipeViewSet(ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+
+    def get_queryset(self):
+        params = self.request.query_params
+        if {'in', 'out'}.intersection(params):
+            return Recipe.objects.filter(
+                items__item__name__contains=params.get('in', ''),
+                items__is_result=False
+            ).filter(
+                items__item__name__contains=params.get('out', ''),
+                items__is_result=True
+            )
+
+        return Recipe.objects.all()
 
 
 class RecipeItemViewSet(ModelViewSet):
